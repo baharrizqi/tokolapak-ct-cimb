8879c13be3bb21c91b5a67b81244fe83c70ff03e
baharrizqi <m.baharrizqi@gmail.com>
2020-04-27 15:27:53 +0700

    :smile: Menggunakan Button UI di Regis|Login

--- src/views/screens/Auth/AuthScreen.jsx
+++ src/views/screens/Auth/AuthScreen.jsx
@@ -9,70 +9,74 @@ class AuthScreen extends React.Component {
     condition: true
   }
   registerHandler = () => {
-    this.setState({condition:true})
+    this.setState({ condition: true })
   }
   loginHandler = () => {
-    this.setState({condition:false})
+    this.setState({ condition: false })
   }
 
   render() {
     return (
       <div>
-        { 
-        !this.state.condition?
-        (<div className="container">
-          <div className="row mt-5">
-            <div className="col-5">
-              <div>
-                <div className="d-flex">
-                  <input type="button" value="Register" className="btn btn-light mr-3" style={{backgroundColor: "white"}} onClick={this.registerHandler}/>
-                  <input type="button" value="Login" className="btn btn-dark" onClick={this.loginHandler}/>
-                </div>
-                <h3 className="mt-2">Log In</h3>
-                <p className="mt-4">Welcome back,<br /> Please, login to your account</p>
-                <TextField placeholder="Username" className="mt-5" />
-                <TextField placeholder="Password" className="mt-2" />
-                <input type="checkbox" name="" id="" className="mt-2" /> Remember me
-                            <div className="d-flex justify-content-center">
-                  <ButtonUI type="contained" className="mt-4">
-                    Login
+        {
+          !this.state.condition ?
+            (<div className="container">
+              <div className="row mt-5">
+                <div className="col-5">
+                  <div>
+                    <div className="d-flex">
+                      {/* <input type="button" value="Register" className="btn btn-light mr-3" style={{backgroundColor: "white"}} onClick={this.registerHandler}/>
+                  <input type="button" value="Login" className="btn btn-dark" onClick={this.loginHandler}/> */}
+                      <ButtonUI className="mr-3" style={{ backgroundColor: "white", color: "black" }} onClick={this.registerHandler}>Register</ButtonUI>
+                      <ButtonUI style={{ backgroundColor: "black", color: "white" }} onClick={this.loginHandler}>Login</ButtonUI>
+                    </div>
+                    <h3 className="mt-2">Log In</h3>
+                    <p className="mt-4">Welcome back,<br /> Please, login to your account</p>
+                    <TextField placeholder="Username" className="mt-5" />
+                    <TextField placeholder="Password" className="mt-2" />
+                    <input type="checkbox" name="" id="" className="mt-2" /> Remember me
+                  <div className="d-flex justify-content-center">
+                      <ButtonUI type="contained" className="mt-4">
+                        Login
                   </ButtonUI>
+                    </div>
+                    <p className="text-center mt-3" style={{ color: "#3C64B1" }}>Forgot password?</p>
+                  </div>
                 </div>
-                <p className="text-center mt-3" style={{ color: "#3C64B1" }}>Forgot password?</p>
+                <div className="col-7">Picture</div>
+              </div>
+            </div>)
+            :
+            <div className="container">
+              <div className="row mt-5">
+                <div className="col-5">
+                  <div>
+                    <div className="d-flex">
+                      {/* <input type="button" value="Register" className="btn btn-dark mr-3" onClick={this.registerHandler}/>
+                  <input type="button" value="Login" className="btn btn-light" style={{backgroundColor: "white"}} onClick={this.loginHandler}/> */}
+                      <ButtonUI className="mr-3" style={{ backgroundColor: "black", color: "white" }} onClick={this.registerHandler}>Register</ButtonUI>
+                      <ButtonUI style={{ backgroundColor: "white", color: "black" }} onClick={this.loginHandler}>Login</ButtonUI>
+                    </div>
+                    <h3 className="mt-2">Register</h3>
+                    <p className="mt-4">You will get the best recommendation for rent <br /> house in near of you</p>
+                    <TextField placeholder="Name" className="mt-5" />
+                    <TextField placeholder="Email" className="mt-2" />
+                    <TextField placeholder="Password" className="mt-2 fa fa-eye" />
+                    <TextField placeholder="Confirm password" className="mt-2" />
+                    <input type="checkbox" /> <span>I agree to</span> <span style={{ color: "#1e4693" }}>Terms of Use</span>
+                    <div className="d-flex justify-content-center">
+                      <ButtonUI type="contained" className="mt-4">
+                        Register
+                  </ButtonUI>
+                    </div>
+                    <p className="text-center mt-3" style={{ color: "#3C64B1" }}>Forgot password?</p>
+                  </div>
+                </div>
+                <div className="col-7">Picture</div>
               </div>
             </div>
-            <div className="col-7">Picture</div>
-          </div>
-        </div>)
-        :
-        <div className="container">
-          <div className="row mt-5">
-            <div className="col-5">
-              <div>
-                <div className="d-flex">
-                  <input type="button" value="Register" className="btn btn-dark mr-3" onClick={this.registerHandler}/>
-                  <input type="button" value="Login" className="btn btn-light" style={{backgroundColor: "white"}} onClick={this.loginHandler}/>
-                </div>
-                <h3 className="mt-2">Register</h3>
-                <p className="mt-4">You will get the best recommendation for rent <br /> house in near of you</p>
-                <TextField placeholder="Name" className="mt-5" />
-                <TextField placeholder="Email" className="mt-2" />
-                <TextField placeholder="Password" className="mt-2 fa fa-eye" />
-                <TextField placeholder="Confirm password" className="mt-2" />
-               <input type="checkbox"/> <span>I agree to</span> <span style={{color: "#1e4693"}}>Terms of Use</span>
-                <div className="d-flex justify-content-center">
-                  <ButtonUI type="contained" className="mt-4">
-                    Register
-                  </ButtonUI>
-                </div>
-                <p className="text-center mt-3" style={{ color: "#3C64B1" }}>Forgot password?</p>
-              </div>
-            </div>
-            <div className="col-7">Picture</div>
-          </div>
-        </div>
-      
-      }
+
+        }
       </div>
 
 
